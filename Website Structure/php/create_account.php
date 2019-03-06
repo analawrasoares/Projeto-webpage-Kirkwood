39927ad72bc783d7d1bd94b6bee373b2579f553b
touhidulIslamMilon <touhidulislammilon4@gmail.com>
2019-03-05 20:55:28 -0600

    Update create_account.php

--- Website Structure/php/create_account.php
+++ Website Structure/php/create_account.php
@@ -7,21 +7,23 @@
  */
 require('form_info.php');
 if(isset($_POST['submit'])){
-    $graduate_year = $_POST['graduate_year'];
+    $graduate_year =htmlspecialchars( $_POST['graduate_year']);
 
-    $salary = $_POST['salary'];
-    $employment_position = $_POST['employment_position'];
+    $salary =htmlspecialchars( $_POST['salary']);
+    $employment_position = htmlspecialchars($_POST['employment_position']);
 
-    $country = $_POST['country'];
-    $city = $_POST['city'];
-    $zip_code = $_POST['zip_code'];
+    $country = htmlspecialchars($_POST['country']);
+    $city = htmlspecialchars($_POST['city']);
+    $zip_code = htmlspecialchars($_POST['zip_code']);
 
-    $program_name = $_POST['program_id'];
-    $employer_name = $_POST['employer_name'];
-    $continue_edu = $_POST['continue_edu'];
-    $state = $_POST['states'];
+    $program_name = htmlspecialchars($_POST['program_id']);
+    $employer_name = htmlspecialchars($_POST['employer_name']);
+    $continue_edu = htmlspecialchars($_POST['continue_edu']);
+    $state = htmlspecialchars($_POST['states']);
+    $continue_edu = htmlspecialchars($_POST['continue_edu']);
     //$location = $_POST['location'];
     $form_info = new form_info($graduate_year,$salary,$employment_position,$program_name,$employer_name,$continue_edu,$country,$state,$city,$zip_code);
+    $form_info->insert();
     echo "Program Name: ".$form_info->getProgram_name() ;
     echo "<br>Graduate Year: ".$form_info->getGraduate_year();
     echo "<br>Salary: ".$form_info->getSalary();
