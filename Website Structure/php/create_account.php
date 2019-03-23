5730cb6cf5aa5719f1ce347e55aa516e9f4c1a7f
touhidulIslamMilon <touhidulislammilon4@gmail.com>
2019-03-22 22:41:01 -0500

    Update create_account.php

--- Website Structure/php/create_account.php
+++ Website Structure/php/create_account.php
@@ -1,41 +1,18 @@
-
 <?php
-/**
- * Author: Md Touhidul Islam
- * Date: 02/25/2019
- * Discription: 
- */
-require('form_info.php');
 require('validation.php');
-if(isset($_POST['submit'])){
-    $graduate_year = isempty(htmlspecialchars( $_POST['graduate_year']),'1');
-
-    $salary = isempty(htmlspecialchars( $_POST['salary'],'1'),'1');
-    $employment_position = isLetter(isempty(htmlspecialchars($_POST['employment_position']),'2'),'2');
-
-    $country =isLetter(isempty(htmlspecialchars($_POST['country']),'3'),'3');
-    $city = isempty(htmlspecialchars($_POST['city']),'4');
-    $zip_code = isempty(htmlspecialchars($_POST['zip_code']),'5');
-
-
-    $program_name = isLetter(isempty(htmlspecialchars($_POST['program_id']),'6'),'6');
-    $employer_name = isLetter(isempty(htmlspecialchars($_POST['employer_name']),'7'),'7');
-    $continue_edu = isempty(htmlspecialchars($_POST['continue_edu']),'8');
-    $continue_edu = isempty(htmlspecialchars($_POST['continue_edu']),'9');
-
-    $program_name = htmlspecialchars($_POST['program_id']);
-    $employer_name = htmlspecialchars($_POST['employer_name']);
-    $continue_edu = htmlspecialchars($_POST['continue_edu']);
-    if(isset($_POST['states'])){
-        $state = isempty(htmlspecialchars($_POST['states']),'10');
-    }else{
-        $state = " ";
-    }
-    $continue_edu = htmlspecialchars($_POST['continue_edu']);
-    //$location = $_POST['location'];
-    $form_info = new form_info($graduate_year,$salary,$employment_position,$program_name,$employer_name,$continue_edu,$country,$state,$city,$zip_code);
-    $form_info->insert();
-    
+require('graduate.php');
+if(isset($_POST['next'])){
+    $k_number = isempty(htmlspecialchars($_POST['k_number']),1);
+    $first_name = isempty(htmlspecialchars($_POST['first_name']),2);
+    $middle_name = isempty(htmlspecialchars($_POST['Middle_name']),3);
+    $last_name =isempty(htmlspecialchars($_POST['last_name']),4);
+    $email = isempty(htmlspecialchars($_POST['email']),5);
+$graduate = new graduate($k_number,$first_name,$middle_name,$last_name,$email);
+$upload_success = $graduate->insert_graduate();
+    if($upload_success){
+        $graduate_id = $graduate->getGraduate_id();
+        header("Location:../Graduate_form.html?graduate_number=$graduate_id");
+    }   
+echo $k_number.$first_name.$middle_name.$last_name.$email;     
 }
-    
 ?>
