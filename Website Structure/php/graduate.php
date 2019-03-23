741af4a8758a115631673035ede1729687049d56
touhidulIslamMilon <touhidulislammilon4@gmail.com>
2019-03-22 22:41:08 -0500

    Update graduate.php

--- Website Structure/php/graduate.php
+++ Website Structure/php/graduate.php
@@ -4,11 +4,14 @@
  * Date: 02/25/2019
  * Discription: 
  */
+require("Helper.php");
 class graduate{
     private $k_number;
     private $first_name;
     private $last_name;
     private $email;
+    private $middle_name;
+    private $graduate_id;
     /**
      * This is the main constructure of graduate class
      *
@@ -17,15 +20,27 @@ class graduate{
      * @param [type] $last_name
      * @param [type] $email
      */
-    function __construct($k_number,$first_name,$last_name,$email) {
-        $this->$k_number = $k_number;
-        $this->$first_name = $first_name;
-        $this->$last_name = $last_name;
-        $this->$email = $email;
+    function __construct($k_number,$first_name,$middle_name,$last_name,$email) {
+        $this->k_number = $k_number;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->email = $email;
+        $this->middle_name = $middle_name;
     }
 
     
-
+    public function insert_graduate(){
+        $conn = connect_db("Kirkwood_Survey");
+        $sql = "INSERT IGNORE INTO graduate (k_number,first_name,middle_name,last_name,email)VALUES ('$this->k_number','$this->first_name','$this->middle_name','$this->last_name','$this->email') ;";
+        if ($conn->multi_query($sql) === TRUE) {
+                echo "New record created successfully";
+            return true;
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                return false;
+            }
+        $conn->close();
+    }
     /**
      * Get the value of k_number
      */ 
@@ -104,6 +119,49 @@ class graduate{
         $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * Get the value of middle_name
+     */ 
+    public function getMiddle_name()
+    {
+        return $this->middle_name;
+    }
+
+    /**
+     * Set the value of middle_name
+     *
+     * @return  self
+     */ 
+    public function setMiddle_name($middle_name)
+    {
+        $this->middle_name = $middle_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of graduate_id
+     */ 
+    public function getGraduate_id()
+    {
+        $conn = connect_db("Kirkwood_Survey");
+        $sql = "SELECT graduate_id FROM graduate WHERE k_number='$this->k_number'";
+
+        $result = $conn->query($sql);
+        $graduate_id='';
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $graduate_id=$row['graduate_id'];
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $conn->close();
+        return $graduate_id;
     }
 }
 ?>
