310bfa960979d82ce4620643e477d79d2930c4d2
touhidulIslamMilon <touhidulislammilon4@gmail.com>
2019-04-16 01:25:45 -0500

    Update form_info.php

--- Website Structure/php/form_info.php
+++ Website Structure/php/form_info.php
@@ -59,33 +59,7 @@ class form_info{
         $this->country_code = "";
     }
 
-    public function insert(){
-            $conn = connect_db("Kirkwood_Survey");
-            $date = date("Ymd");
-
-            $sql = "INSERT IGNORE INTO State (state_name)VALUES ('$this->state') ;";
-            $sql .= "INSERT IGNORE INTO Location (city, zip, state_name , country_code)VALUES ('$this->city', '$this->zip_code', (SELECT state_name FROM state WHERE state_name = '$this->state' ), (SELECT country_code FROM country WHERE country_code = '$this->country' ));";
-            $sql .= "INSERT IGNORE INTO program (program_name)VALUES ('$this->program_name');";
-            
-
-            if ($conn->multi_query($sql) === TRUE) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-
-            $newsql = "INSERT INTO Form (graduate_id, graduate_year, salary_range, employment_position, location_id, submission_date, program_name, employer_name, continue_edu, have_job)VALUES ('$this->graduate_id' , '$this->graduate_year' , '$this->salary' , '$this->employment_position' , (SELECT location_id FROM Location WHERE zip = '$this->zip_code' ),'$date','$this->program_name','$this->employer_name','$this->continue_edu',true);";
-            $conn->close();
-            $conn = connect_db("Kirkwood_Survey");
-            if ($conn->query($newsql) === TRUE) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $newsql . "<br>" . $conn->error;
-            }
-
-
-            $conn->close();
-    }
+    
     /**
      * Get the value of graduate_year
      */ 
